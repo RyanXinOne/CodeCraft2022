4597cc787534a1d7f7d863e9a8e4b95890e6308e
RyanXin <xyz@ryanxin.cn>
2022-03-13 21:18:35 +0000

    Add basic io template codes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CodeCraft2022
-Team repo of HUAWEI CodeCraft 2022
+Team 放浪不羁之客 - repo of HUAWEI CodeCraft 2022
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool debug = true;
+
+vector<int> parseLine(string line, char delimiter = ',')
+{
+    vector<int> result;
+    int current = 0;
+    for (int i = 0; i < line.size(); i++)
+    {
+        if (line[i] == delimiter)
+        {
+            result.push_back(current);
+            current = 0;
+        }
+        else
+        {
+            current = current * 10 + (line[i] - '0');
+        }
+    }
+    result.push_back(current);
+    return result;
+}
+
+int main()
+{
+    if (debug)
+    {
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    }
+
+    string line;
+    cin >> line;
+    vector<int> numbers = parseLine(line);
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        cout << numbers[i] << ' ';
+    }
+
+    return 0;
+}
