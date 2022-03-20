057aaf5eb03b17f7b20ee843330705b1d0a3c747
Gnefil <lifeng2628qiu@gmail.com>
2022-03-20 17:44:45 +0000

    Rearrage type errors in allocate demand to node

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -7,9 +7,9 @@ using namespace std;
 bool DEBUG = true;
 
 // Calculate given a client and time t, sum of the accessible nodes capacity left (ancls)
-unsigned accessible_nodes_left_capacity_sum(client &client, int t, vector<node> &nodes)
+int accessible_nodes_left_capacity_sum(client &client, int t, vector<node> &nodes)
 {
-    unsigned sum = 0;
+    int sum = 0;
 
     for (auto node_id : client.accessible_nodes)
     {
@@ -30,13 +30,13 @@ bool compare_ancls(vector<int, unsigned> v1, vector<int, unsigned> v2)
 void allocate_demands_to_node(vector<client> &clients, vector<node> &nodes, int t, int node_id)
 {
     // Prepare a 2D vector for customise sorting
-    vector<vector<int, unsigned>> id_and_anlcs;
+    vector<vector<int>> id_and_anlcs;
 
     for (auto client_id : nodes[node_id].accessible_clients)
     {
 
         // Store id, anlcs
-        vector<int, unsigned> temp{client_id, accessible_nodes_left_capacity_sum(clients[client_id], t, nodes)};
+        vector<int> temp {client_id, accessible_nodes_left_capacity_sum(clients[client_id], t, nodes)};
         id_and_anlcs.push_back(temp);
     }
 
@@ -47,20 +47,20 @@ void allocate_demands_to_node(vector<client> &clients, vector<node> &nodes, int 
     for (auto i = 0; i < id_and_anlcs.size(); i++)
     {
         int client_id = id_and_anlcs[i][0];
-        int left_capacity = nodes[node_id].capacity - nodes[node_id].allocated;
+        unsigned left_capacity = nodes[node_id].capacity - nodes[node_id].allocated[t];
         int client_demand = clients[client_id].demands[t];
 
         // If all capacity needed
         if (left_capacity <= client_demand)
         {
             clients[client_id].demands[t] -= left_capacity;
-            clients[client_id].allocations.push_back(node_id, left_capacity);
+            clients[client_id].allocations[t][node_id] = left_capacity;
             nodes[node_id].allocated[t] = nodes[node_id].capacity;
             break;
         }
 
         clients[client_id].demands[t] = 0;
-        clients[client_id].allocations.push_back(node_id, client_demand);
+        clients[client_id].allocations[t][node_id] = client_demand;
         nodes[node_id].allocated[t] += client_demand;
     }
 }
