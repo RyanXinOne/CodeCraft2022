57ff54ab401d815122e5a6b4f7166f1185ab36d4
RyanXin <xyz@ryanxin.cn>
2022-03-23 17:04:08 +0000

    Fix main logic

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -8,84 +8,89 @@ using namespace std;
 
 bool DEBUG = true;
 
-// Calculate given a client and time t, sum of the accessible nodes capacity left (ancls)
-int accessible_nodes_left_capacity_sum(client &client, size_t t, vector<node> &nodes)
+// Calculate given a client and time t, sum of the accessible nodes capacity left (sum of available capacity)
+int client_priority(client &client, size_t t, vector<node> &nodes)
 {
-    int sum = 0;
+    unsigned sum = 0;
 
     for (int node_id : client.accessible_nodes)
     {
         sum += nodes[node_id].capacity - nodes[node_id].allocated[t];
     }
 
-    return sum;
-}
-
-// Customised compare method
-bool compare_ancls(vector<int> &v1, vector<int> &v2)
-{
-    return v1[1] < v2[1];
+    return (int)sum;
 }
 
 // at time t, allocate demands to input node as many as possible
 // remember to update clients.demands[t], clients.allocations[t], and nodes.allocated[t]
-void allocate_demands_to_node(vector<client> &clients, vector<node> &nodes, size_t t, int node_id)
+void allocate_demands_to_node(int node_id, size_t t, vector<client> &clients, vector<node> &nodes)
 {
-    // Prepare a 2D vector for customise sorting
-    vector<vector<int>> id_and_anlcs;
+    node &node = nodes[node_id];
 
-    for (int client_id : nodes[node_id].accessible_clients)
+    // prepare a pair vector for customise sorting
+    vector<vector<int>> cid_w_prio;
+
+    for (int client_id : node.accessible_clients)
     {
-        // Store id, anlcs
-        vector<int> temp{client_id, accessible_nodes_left_capacity_sum(clients[client_id], t, nodes)};
-        id_and_anlcs.push_back(temp);
+        if (clients[client_id].demands[t] == 0)
+            continue;
+        // store id, anlcs
+        vector<int> pair{client_id, client_priority(clients[client_id], t, nodes)};
+        cid_w_prio.push_back(pair);
     }
 
-    // Sort accessible clients by sum of their accessible nodes capacity left in ASC
-    sort(id_and_anlcs.begin(), id_and_anlcs.end(), compare_ancls);
+    // sort accessible clients by their priorities in ASC
+    sort(cid_w_prio.begin(), cid_w_prio.end(), [](vector<int> &v1, vector<int> &v2)
+         { return v1[1] < v2[1]; });
 
-    // Allocate demands
-    for (size_t i = 0; i < id_and_anlcs.size(); i++)
+    // allocate demands
+    for (size_t i = 0; i < cid_w_prio.size(); i++)
     {
-        int client_id = id_and_anlcs[i][0];
-        unsigned left_capacity = nodes[node_id].capacity - nodes[node_id].allocated[t];
-        unsigned client_demand = clients[client_id].demands[t];
+        int client_id = cid_w_prio[i][0];
+        unsigned allo_amount = min(node.capacity - node.allocated[t], clients[client_id].demands[t]);
 
-        // If all capacity needed
-        if (left_capacity <= client_demand)
+        clients[client_id].demands[t] -= allo_amount;
+        // check and initialise allocation
+        unordered_map<int, unsigned> &allocation = clients[client_id].allocations[t];
+        if (allocation.find(node_id) == allocation.end())
         {
-            clients[client_id].demands[t] -= left_capacity;
-            clients[client_id].allocations[t][node_id] = left_capacity;
-            nodes[node_id].allocated[t] = nodes[node_id].capacity;
-            break;
+            allocation[node_id] = 0;
         }
+        allocation[node_id] += allo_amount;
+        node.allocated[t] += allo_amount;
 
-        clients[client_id].demands[t] = 0;
-        clients[client_id].allocations[t][node_id] = client_demand;
-        nodes[node_id].allocated[t] += client_demand;
+        // if running out of capacity
+        if (node.capacity == node.allocated[t])
+            break;
     }
 }
 
-// Compute the value of the 95% of the node, the cost of the node across whole time series
-unsigned node_95(node &node, vector<client> &clients)
+// Compute distance 95
+unsigned calc_distance_95(node &node, vector<client> &clients)
 {
-    int total_t = clients[0].demands.size();
-    int num_95 = ceil(total_t * 0.95);
-    vector<unsigned> max_allocated_list(total_t);
+    size_t total_t = clients[0].demands.size();
+    size_t index_95 = ceil(total_t * 0.95) - 1;
+    vector<unsigned> max_allocations(total_t);
 
-    for (int t = 0; t < total_t; t++)
+    for (size_t t = 0; t < total_t; t++)
     {
         unsigned sum = 0;
-        for (client client : clients)
+        for (int client_id : node.accessible_clients)
         {
-            sum += client.demands[t];
+            sum += clients[client_id].demands[t];
         }
-        max_allocated_list.push_back(sum);
+        max_allocations.push_back(min(sum, node.capacity));
+    }
+    nth_element(max_allocations.begin(), max_allocations.begin() + index_95, max_allocations.end());
+
+    unsigned bandwidth_95 = max_allocations[index_95];
+    unsigned distance_95 = 0;
+    for (size_t i = 0; i < index_95; i++)
+    {
+        distance_95 += bandwidth_95 - max_allocations[i];
     }
 
-    sort(max_allocated_list.begin(), max_allocated_list.end());
-
-    return max(node.capacity, max_allocated_list[num_95]);
+    return distance_95;
 }
 
 // Schedule the demands of the clients to the nodes on min_95 strategy
@@ -100,31 +105,27 @@ void schedule_traffic(vector<client> &clients, vector<node> &nodes)
     while (!remainingNodes.empty())
     {
         // Initialize values
-        int min_95_i = *(remainingNodes.begin());
-        unsigned min_95 = node_95(nodes[min_95_i], clients);
+        int min_dis_95_i = -1;
+        unsigned min_dis_95 = numeric_limits<unsigned>::max();
 
         // Select node with minimum 95 cost
         for (int i : remainingNodes)
         {
-            unsigned cur_95 = node_95(nodes[i], clients);
-            if (cur_95 < min_95)
+            unsigned cur_95_dis = calc_distance_95(nodes[i], clients);
+            if (cur_95_dis < min_dis_95)
             {
-                min_95_i = i;
-                min_95 = cur_95;
+                min_dis_95_i = i;
+                min_dis_95 = cur_95_dis;
             }
         }
 
         // Allocate demands to node for each time t
         for (size_t t = 0; t < clients[0].demands.size(); t++)
         {
-            allocate_demands_to_node(clients, nodes, t, min_95_i);
+            allocate_demands_to_node(min_dis_95_i, t, clients, nodes);
         }
 
-        remainingNodes.erase(min_95_i);
-        if (DEBUG)
-        {
-            cout << remainingNodes.size() << endl;
-        }
+        remainingNodes.erase(min_dis_95_i);
     }
 
     // Need to check if all demand is allocated
