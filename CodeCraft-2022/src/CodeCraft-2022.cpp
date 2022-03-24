b6113ec30d542f77ba8780c08c6df6624a1c06e0
Gnefil <lifeng2628qiu@gmail.com>
2022-03-24 11:08:21 +0000

    Start optimize

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cmath>
 #include <unordered_set>
+#include <cstdlib>
 
 using namespace std;
 
@@ -149,6 +150,25 @@ void schedule_traffic(vector<client> &clients, vector<node> &nodes)
             }
         }
     }
+}
+
+// Choose node to be reduced
+int select_reduce (vector<node> &nodes)
+{
+    return rand() % nodes.size();
+}
+
+// Choose node to be reduced
+int select_increase (vector<node> &nodes)
+{
+    return rand() % nodes.size();
+}
+
+// Optimize by random node's 95 distance (changing allocations where node before receives less demand; node after receives more demand)
+void optimize(vector<client> &clients, vector<node> &nodes) {
+    int reduce_node_i = select_reduce(nodes);
+    int increase_node_i = select_increase(nodes);
+    
 }
 
 int main()
